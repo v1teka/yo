8b7b9bda949e06e8803c1dc762e0c032b3030369
v1teka <v1teka@mail.ru>
2017-10-01 19:42:02 +0700

    Дописал страницу информации

--- resources/views/onebutton.blade.php
+++ resources/views/onebutton.blade.php
@@ -37,32 +37,50 @@
                     url: "informationport?ip="+ip+"&t=3"
                 });
             }
+
+            function message(ip){
+                $.ajax({
+                    url: "informationport?ip="+ip+"&t=4&message="+$("#messageinput").val();
+                });
+            }
         </script>
     </head>
     <body>
+        <input id="messageinput" placeholder="Сообщение" />
+        <div>
+            <p>local</p>
+            <button onClick="getinfo('127.0.0.1')">Информация</button>
+            <button onClick="getscreen('127.0.0.1')">Скриншот</button>
+            <button disabled onClick="shutdown('127.0.0.1')">Выключить нахуй</button>
+            <button disabled onClick="message('127.0.0.1')">Сообщение</button>
+        </div>
         <div>
             <p>vitya</p>
-            <button onClick="zaprosec('192.168.1.39')">Информация</button>
-            <button onClick="zaproscreen('192.168.1.39')">Скриншот</button>
+            <button onClick="getinfo('192.168.1.39')">Информация</button>
+            <button onClick="getscreen('192.168.1.39')">Скриншот</button>
             <button disabled onClick="shutdown('192.168.1.39')">Выключить нахуй</button>
+            <button disabled onClick="message('192.168.1.39')">Сообщение</button>
         </div>
         <div>
             <p>Kostyan</p>
             <button onClick="zaprosec('192.168.1.53')">Информация</button>
             <button onClick="zaproscreen('192.168.1.53')">Скриншот</button>
             <button disabled onClick="shutdown('192.168.1.53')">Выключить нахуй</button>
+            <button disabled onClick="message('192.168.1.53')">Сообщение</button>
         </div>
         <div>
             <p>vovanja</p>
             <button onClick="zaprosec('192.168.1.58')">Информация</button>
             <button onClick="zaproscreen('192.168.1.58')">Скриншот</button>
             <button disabled onClick="shutdown('192.168.1.58')">Выключить нахуй</button>
+            <button disabled onClick="message('192.168.1.58')">Сообщение</button>
         </div>
         <div>
             <p>vitalya</p>
             <button onClick="zaprosec('192.168.1.69')">Информация</button>
             <button onClick="zaproscreen('192.168.1.69')">Скриншот</button>
             <button disabled onClick="shutdown('192.168.1.69')">Выключить нахуй</button>
+            <button disabled onClick="message('192.168.1.69')">Сообщение</button>
         </div>
         <p id="info"></p>
         <img id="image" style="max-width: 800px"/>
