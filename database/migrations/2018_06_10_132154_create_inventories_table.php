a9ffcc5592a785870276034f59a4a277b44e0bb7
VitalyEfremov <vvedeveloper@gmail.com>
2018-06-10 21:26:17 +0700

    Added new migration

---
+++ database/migrations/2018_06_10_132154_create_inventories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->unsignedInteger('number')->nullable()->primary();
+            $table->unsignedSmallInteger('type_id')->nullable();
+            $table->unsignedSmallInteger('classroom_id')->nullable();
+            $table->integer('x')->nullable();
+            $table->integer('y')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
